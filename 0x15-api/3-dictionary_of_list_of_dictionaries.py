c8c29f7793345950c71df955017da791ab97f457
Farah <farah.mccurdy@gmail.com>
2022-09-12 04:58:27 -0700

    add: file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" export data in JSON format"""
+
+import json
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    r_employee = get('https://jsonplaceholder.typicode.com/users/')
+    employee = r_employee.json()
+
+    user_dict = {}
+    for e in employee:
+        todo_list = []
+        id = e['id']
+        r_todo = get("https://jsonplaceholder.typicode.com/todos/?userId={}".
+                     format(id))
+        todo = r_todo.json()
+        for task in todo:
+            todo_dict = {}
+            todo_dict["username"] = e['username']
+            todo_dict["task"] = task['title']
+            todo_dict["completed"] = task['completed']
+            todo_list.append(todo_dict)
+        user_dict[id] = todo_list
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(user_dict, json_file)
