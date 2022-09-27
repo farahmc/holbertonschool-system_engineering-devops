a3fe5882fc102d5d061c3e9749f4c701de1a57cd
Farah <farah.mccurdy@gmail.com>
2022-09-27 03:28:27 +0000

    add: file

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" queries the Reddit API and prints the titles of the
+ first 10 hot posts listed for a given subreddit """
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    """ return titles of first 10 hot posts """
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)\
+            AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(argv[1])
+    r_hot = requests.get(url, headers=headers)
+    if r_hot.ok is False:
+        return None
+
+    hot_posts = r_hot.json()
+    i = 0
+    top_10 = []
+    while i < 10:
+        top_10.append(hot_posts['data']['children'][i]['data']['title'])
+        i += 1    
+    for title in top_10:
+        print(title)
