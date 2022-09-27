badef73bb63994dfa41af1647e0c7bec87503146
Farah <farah.mccurdy@gmail.com>
2022-09-27 06:00:27 +0000

    add: do not allow redirects

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)\
             AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    r_hot = requests.get(url, headers=headers)
+    r_hot = requests.get(url, headers=headers, allow_redirects=False)
     if r_hot.ok is False:
         print("None")
         return None
