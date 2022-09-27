db1fb846eddf7301bebe20a953d132a36380bc1d
Farah <farah.mccurdy@gmail.com>
2022-09-27 03:41:51 +0000

    add: pydocs

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,9 @@ from sys import argv
 
 
 def top_ten(subreddit):
-    """ return titles of first 10 hot posts """
+    """ return titles of first 10 hot posts
+    if not a valid subreddit, print None 
+    do not follow redirects """
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)\
             AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
     url = "https://www.reddit.com/r/{}/hot.json".format(argv[1])
